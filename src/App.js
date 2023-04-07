681cb4611748eaa3a5f645c6627b7f72a47cce91
nerdnightboy <nerdnightboy@.com>
2023-04-07 12:50:24 +0530

    Just Starting with react project of building an e- commerce website name go-grocery.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='*' element={<div>
+            <h1>H-A-H-A Seems Like You Don't Have Enough Knowledge to Access US.</h1>
+          </div>} />
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
