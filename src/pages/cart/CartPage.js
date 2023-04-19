dd2b9f669834e5f9a78ba3b9939a7905f3662e0b
nerdnightboy <nerdnightboy@.com>
2023-04-19 15:45:26 +0530

    SOme functionality added to the cart page

--- src/pages/cart/CartPage.js
+++ src/pages/cart/CartPage.js
@@ -5,6 +5,7 @@ import Footer1 from '../../components/footer/Footer1'
 import Footer2 from '../../components/footer/Footer2'
 import SingleBanner from '../../components/baner/SingleBanner'
 import './Progress.css'
+import './CartPageContainer.css'
 import { Link } from 'react-router-dom'
 
 
@@ -44,6 +45,10 @@ const CartPage = () => {
 
   const checklogin = () => {
     return true
+  }
+
+  const removeitemfromcart = (index) => {
+    alert(index)
   }
   return (
     <div>
@@ -159,14 +164,126 @@ const CartPage = () => {
 
         {
           active == 1 &&
-          <div className='cartcont'>
-            <p>Cart Cont</p>
+          <div className='container'>
+            {
+              cartdata.length > 0 ?
+                <table className='carttable'>
+                  <thead>
+                    <tr>
+                      <th>Product</th>
+                      <th>Quantity</th>
+                      <th>Price</th>
+                      <th>Total</th>
+                      <th>Remove</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      cartdata.map((item, index) => {
+                        return (
+                          <tr key={index} className='cartitemrow'>
+                            <td >
+                              <div className='cartproduct' onClick={() => {
+                                window.location.href = '/product/${item.productdata.productId}'
+                              }}>
+                              <img src={item.productdata.productImage[0].image} alt={item.productdata.productName} />
+                              <p>{
+                                item.productdata.productName
+                              }</p>
+                              </div>
+                            </td>
+                            <td calssName='quantity'>
+                              <button className='minus' onClick={() => {
+                                let newcartdata = [...cartdata]
+
+                                if(newcartdata[index].quantity >1){
+                                  newcartdata[index].quantity -= 1
+                                  setcartdata(newcartdata)
+                                  localStorage.setItem('cart', JSON.stringify(newcartdata))
+                                  getcartitemsfromlocalstorage()
+                                }
+                              }}>-</button>
+                              <span>{item.quantity}</span>
+                              <button className='plus' onClick={() => {
+                                let newcartdata = [...cartdata]
+                                  newcartdata[index].quantity += 1
+                                  setcartdata(newcartdata)
+                                  localStorage.setItem('cart', JSON.stringify(newcartdata))
+                                  getcartitemsfromlocalstorage()
+                              }}>+</button>
+                            </td>
+
+                            <td>
+                              <p>$ {item.productdata.salesPrice ? item.productdata.salesPrice.toFixed(2) : 0.00} </p>
+                            </td>
+
+                            <td>
+                              <p>
+                                $ {
+                                  (item.productdata.salesPrice * item.quantity).toFixed(2)
+                                }
+                              </p>
+                            </td>
+
+                            <td>
+                              <div className='deletebtn' onClick={() => {
+                                removeitemfromcart(index)
+                              }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+</svg>
+
+                              </div>
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td className='totaltableleft'>Sub-Total</td>
+                      <td className='totaltableright'>{subtotal.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td className='totaltableleft'>Shipping</td>
+                      <td className='totaltableright'>{shipping.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td className='totaltableleft'>Total</td>
+                      <td className='totaltableright'>{(subtotal + shipping).toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td className='totaltableleft'>Tax</td>
+                      <td className='totaltableright'>{tax.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td className='totaltableleft'>Net-Total</td>
+                      <td className='totaltableright'>{(tax + subtotal + shipping).toFixed(2)}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                :
+                <div className='emptycart'>
+                  <p>Your cart is empty</p>
+                </div>
+            }
           </div>
         }
         {
           active == 2 &&
           <div className='shippingcont'>
             <p>Shipping Cont</p>
+
           </div>
         }
         {
@@ -217,16 +334,16 @@ const CartPage = () => {
           </div>
         }
 
-{
+        {
           active == 4 &&
           <div className='btns'>
             <button className='backbtn' onClick={() => {
               checklogin() && setactive(3)
             }}>Back</button>
             <Link to={'/'}>
-            <button className='nextbtn' onClick={() => {
-              alert('Order is successfully placed')
-            }}>Go to Home</button>
+              <button className='nextbtn' onClick={() => {
+                alert('Order is successfully placed')
+              }}>Go to Home</button>
             </Link>
           </div>
         }
